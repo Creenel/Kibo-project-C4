eb4afb65424baf1aaddf570074cca3eaf9bcd8c0
rotemre25-meet <chinkycreenel@gmail.com>
2024-07-25 15:40:38 +0300

    setup no firebase yet

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import pyrebase
 
 app = Flask(__name__,template_folder = 'templates', static_folder = "static")
 app.config["SECRET_KEY"] = "rotemthebest"
+
+if (__name__) == "__main__":
+	app.run(debug = True)
