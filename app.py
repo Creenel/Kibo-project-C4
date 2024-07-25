eea4f1fc1749416207da100378a43cbcc7048481
rotemre25-meet <55294415+Creenel@users.noreply.github.com>
2024-07-25 15:44:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,15 @@ import pyrebase
 app = Flask(__name__,template_folder = 'templates', static_folder = "static")
 app.config["SECRET_KEY"] = "rotemthebest"
 
+fbConfig = {
+  apiKey: "AIzaSyATvFI4Z5nj5VKP1Y16JGnAN2LcIk7ECUI",
+  authDomain: "kibo-project-8ea43.firebaseapp.com",
+  projectId: "kibo-project-8ea43",
+  storageBucket: "kibo-project-8ea43.appspot.com",
+  messagingSenderId: "1010392688942",
+  appId: "1:1010392688942:web:b334a7bf76efa442e8f2bf",
+  measurementId: "G-XFHXVES630",
+};
+
 if (__name__) == "__main__":
 	app.run(debug = True)
