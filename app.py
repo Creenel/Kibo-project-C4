61791a651f075b069151ddb6b4e25a17c3f46ba8
halaat24-meet <halaatari3@gmail.com>
2024-07-29 09:38:42 +0300

    html

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = Flask(__name__,template_folder = 'templates', static_folder = "static")
 app.config["SECRET_KEY"] = "rotemthebest"
 
 fbConfig = {
-  "apiKey": "AIzaSyATvFI4Z5nj5VKP1Y16JGnAN2LcIk7ECUI",
-  "authDomain": "kibo-project-8ea43.firebaseapp.com",
-  "projectId": "kibo-project-8ea43",
-  "storageBucket": "kibo-project-8ea43.appspot.com",
-  "messagingSenderId": "1010392688942",
-  "appId": "1:1010392688942:web:b334a7bf76efa442e8f2bf",
-  "measurementId": "G-XFHXVES630",
-  "databaseURL":"https://kibo-project-8ea43-default-rtdb.europe-west1.firebasedatabase.app/"}
+	"apiKey": "AIzaSyATvFI4Z5nj5VKP1Y16JGnAN2LcIk7ECUI",
+	"authDomain": "kibo-project-8ea43.firebaseapp.com",
+	"projectId": "kibo-project-8ea43",
+	"storageBucket": "kibo-project-8ea43.appspot.com",
+	"messagingSenderId": "1010392688942",
+	"appId": "1:1010392688942:web:b334a7bf76efa442e8f2bf",
+	"measurementId": "G-XFHXVES630",
+	"databaseURL":"https://kibo-project-8ea43-default-rtdb.europe-west1.firebasedatabase.app/"}
 
 firebase = pyrebase.initialize_app(fbConfig)
 auth = firebase.auth()
